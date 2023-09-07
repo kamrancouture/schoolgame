fad9feff8fe7acb6297e8c19d570d9082420254a
kamrancouture <65361792+kamrancouture@users.noreply.github.com>
2023-09-06 17:56:24 -0700

    gun in hand

--- schoolgame/Player.gd
+++ schoolgame/Player.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-
+var gun_in_hand = false
 var velocity = Vector2.ZERO
 var speed = 600
 
@@ -9,3 +9,12 @@ func _physics_process(delta):
 	move_and_slide(velocity)
 	look_at(get_global_mouse_position())
 	
+	if Input.is_action_just_pressed("gun") and not gun_in_hand:
+		gun_in_hand = true
+		speed /= 1.5
+		$AnimatedSprite.play("pistol")
+	elif Input.is_action_just_pressed("gun"):
+		gun_in_hand = false
+		speed *= 1.5
+		$AnimatedSprite.play("idle")
+
