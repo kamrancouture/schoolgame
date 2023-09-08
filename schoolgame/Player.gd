65fcd330bbf15f7c0b5e7c658954d9d3fef4701c
kamrancouture <65361792+kamrancouture@users.noreply.github.com>
2023-09-08 11:58:07 -0700

    gun works now

--- schoolgame/Player.gd
+++ schoolgame/Player.gd
@@ -1,8 +1,15 @@
 extends KinematicBody2D
 
+export var fire_rate = 0.2
+var Player_Bullet = preload("res://player_bullet.tscn")
+var can_shoot = true
 var gun_in_hand = false
 var velocity = Vector2.ZERO
-var speed = 600
+var speed = 450
+var rng = RandomNumberGenerator.new()
+
+func _ready():
+	$AnimatedSprite.play("idle")
 
 func _physics_process(delta):
 	velocity = Input.get_vector("a_click" , "d_click" , "w_click" , "s_click") * speed
@@ -19,4 +26,15 @@ func _physics_process(delta):
 		$AnimatedSprite.play("idle")
 	
 	
-	
+	if Input.is_action_pressed("shoot") and can_shoot and gun_in_hand:
+		can_shoot = false
+		var player_bullet = Player_Bullet.instance()
+		player_bullet.global_position = $bullet_spawn.global_position
+		player_bullet.global_rotation = global_rotation
+		get_parent().add_child(player_bullet)
+		$fire_rate.start(fire_rate)
+		
+
+
+func _on_fire_rate_timeout():
+	can_shoot = true
--- schoolgame/player_bullet.gd
+++ schoolgame/player_bullet.gd
@@ -1,6 +1,5 @@
 extends Area2D
-var speed = 10
+var speed = 15
+
 func _physics_process(delta):
-	
 	global_position += Vector2(speed , 0).rotated(rotation)
-	
