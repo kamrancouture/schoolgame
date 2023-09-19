537db5d6a00d1f0e7e7163dcfc981f85f70601bc
kamrancouture <65361792+kamrancouture@users.noreply.github.com>
2023-09-19 16:24:11 -0700

    text box replay

--- schoolgame/text_popup.gd
+++ schoolgame/text_popup.gd
@@ -2,9 +2,9 @@ extends PanelContainer
 
 
 func pop_up():
-	if not $AnimationPlayer.is_playing():
-		$time_showing.start()
-		show()
+	$AnimationPlayer.play("RESET")
+	$time_showing.start()
+	show()
 
 func _on_time_showing_timeout():
 	$AnimationPlayer.play("fade")
