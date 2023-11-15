71e142cffb1f0d2276b1ada756b3b689716e6953
nocelly <140983116+nocelly@users.noreply.github.com>
2023-11-15 10:22:59 -0800

    music

--- schoolgame/settings_screen.gd
+++ schoolgame/settings_screen.gd
@@ -2,11 +2,14 @@ extends Control
 
 func _ready():
 	$HScrollBar.value = Global.volume
+	$settings_music.play()
 func _physics_process(delta):
 	Global.volume = $HScrollBar.value
-
+	$Label.text = String(int((Global.volume + 80) / 86 * 100)) + "%"
 
 
 
 func _on_Button_button_down():
 	get_tree().change_scene("res://main_screen.tscn")
+
+
