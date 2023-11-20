2fdda1298ede9974dbaa0a0d9c56e25a10e727df
nocelly <140983116+nocelly@users.noreply.github.com>
2023-11-20 10:24:32 -0800

    paper

--- schoolgame/paper.gd
+++ schoolgame/paper.gd
@@ -8,11 +8,11 @@ var player_lowest_non_selected : int
 var player_lowest_non_selected_found = false
 var item_picked_up = false
 func _physics_process(delta):
-	
 	if get_overlapping_bodies():
 		$Press_E.show()
 		$Press_E.global_position = player.global_position + Vector2(7 , -40)
 		if Input.is_action_just_pressed("interact"):
+			Global.XP += 20
 			Global.paper_number += 1
 			queue_free()
 	else:
