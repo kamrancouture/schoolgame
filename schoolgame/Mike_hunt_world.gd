5c27c8d75ebe5b5345dd69deab13c55617638b05
nocelly <140983116+nocelly@users.noreply.github.com>
2023-12-13 11:40:51 -0800

    mike

--- schoolgame/Mike_hunt_world.gd
+++ schoolgame/Mike_hunt_world.gd
@@ -4,4 +4,6 @@ extends Node2D
 
 
 
-
+func _physics_process(delta):
+	if $mikes.get_child_count() == 0:
+		print("hello")
--- schoolgame/mike_hunt.gd
+++ schoolgame/mike_hunt.gd
@@ -4,10 +4,10 @@ extends KinematicBody2D
 var can_hit = true
 var Death_Blood = preload("res://zombie_death_effect.tscn")
 var Blood_Effect = preload("res://zombie_blood_effect.tscn")
-onready var player = get_parent().get_node("Player")
+onready var player = get_parent().get_parent().get_node("Player")
 var aggro = false
 var rng = RandomNumberGenerator.new()
-var health = 10
+var health = 1
 var velocity = Vector2.ZERO
 var speed = 335
 var damage = 0.025
@@ -24,6 +24,11 @@ func _ready():
 	$name_tag.hide()
 
 func _physics_process(delta):
+	if $AnimatedSprite.animation == "hide":
+		$mikeCollisionShape2D.disabled = true
+		$big_mikeCollisionShape2D2.disabled = true
+	if health <= 0:
+		Global.mikes_left += 1
 	move_and_slide(velocity)
 	
 	if Global.world == "computer_class":
@@ -43,24 +48,26 @@ func _physics_process(delta):
 			var death_blood = Death_Blood.instance()
 			death_blood.global_position = global_position
 			death_blood.emitting = true
-			get_parent().add_child(death_blood)
+			get_parent().get_parent().add_child(death_blood)
 			Global.students_alive -= 1
 			queue_free()
 		
 		if aggro:
 			$name_tag.show()
-			if rng.randf_range(0,100) < 30 and not set_sprite:
+			if rng.randf_range(0,100) < 26 and not set_sprite:
 				health += 7
 				damage += 0.5
 				speed += 65
 				set_sprite = true
 				$AnimatedSprite.play("big_mike")
 				$mikeCollisionShape2D.disabled = true
+				$big_mikeCollisionShape2D2.disabled = false
 				$attack_box/mikeCollisionShape2D.disabled = true
 			elif not set_sprite:
 				set_sprite = true
 				$AnimatedSprite.play("default")
 				$big_mikeCollisionShape2D2.disabled = true
+				$mikeCollisionShape2D.disabled = false
 				$attack_box/big_mikeCollisionShape2D2.disabled = true
 			navigation_agent.set_target_location(player.global_position)
 			velocity = global_position.direction_to(navigation_agent.get_next_location()) * speed
@@ -76,7 +83,7 @@ func hit():
 	var blood_effect = Blood_Effect.instance()
 	blood_effect.global_position = global_position
 	blood_effect.emitting = true
-	get_parent().add_child(blood_effect)
+	get_parent().get_parent().add_child(blood_effect)
 
 func _on_Area2D_body_entered(body):
 	$AnimatedSprite.play("aggro")
