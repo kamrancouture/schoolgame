b2dbee8a440d53afe3f1d7651658c0faafa152be
nocelly <140983116+nocelly@users.noreply.github.com>
2023-12-14 11:40:07 -0800

    miiike

--- schoolgame/Mike_hunt_world.gd
+++ schoolgame/Mike_hunt_world.gd
@@ -5,6 +5,9 @@ extends Node2D
 
 
 func _physics_process(delta):
-	if $mikes.get_child_count() == 0:
+	if $mikes.get_child_count() == 0 and $boss_mike.spawned == false:
+		print("hello")
 		$boss_mike.show()
+		$boss_mike/big_mikeCollisionShape2D2.disabled = false
+		$boss_mike/attack_box/big_mikeCollisionShape2D2.disabled = false
 		$boss_mike.spawned = true
--- schoolgame/boss_mike.gd
+++ schoolgame/boss_mike.gd
@@ -1,8 +1,16 @@
 extends KinematicBody2D
 onready var player = get_parent().get_node("Player")
 var spawned = false
-
+var health = 3
 func _physics_process(delta):
+	print($big_mikeCollisionShape2D2.disabled)
 	if spawned:
-		look_at(player.global_position)
-	
+		$Sprite.look_at(player.global_position)
+		if health <= 0:
+#			$attack_box/big_mikeCollisionShape2D2.disabled = true
+#			$big_mikeCollisionShape2D2.disabled = true
+			hide()
+			set_physics_process(false)
+func hit():
+	pass
+
