611183dfd2d2114531380e82f0d8420786202121
kamrancouture <65361792+kamrancouture@users.noreply.github.com>
2023-12-18 11:34:08 -0800

    fixed tetris pieces

--- schoolgame/Tetris_world.gd
+++ schoolgame/Tetris_world.gd
@@ -1,13 +1,12 @@
 extends Node2D
 
-var I_Block = preload("res://Assets/tetris/pieces/I_Block.png")
+var I_Block = preload("res://Assets/tetris/pieces/I-Block.png")
 var T_Block = preload("res://Assets/tetris/pieces/T_Block.png")
 var O_Block = preload("res://Assets/tetris/pieces/O_Block.png")
 var S_Block = preload("res://Assets/tetris/pieces/S_Block.png")
 var Z_Block = preload("res://Assets/tetris/pieces/Z_Block.png")
 var J_Block = preload("res://Assets/tetris/pieces/J_Block.png")
 var L_Block = preload("res://Assets/tetris/pieces/L_Block.png")
-
 
 
 var pieces = [
