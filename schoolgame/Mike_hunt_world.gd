e0236b58f9679980cc0519fadc59c36bc8a999ed
nocelly <140983116+nocelly@users.noreply.github.com>
2024-01-10 11:39:53 -0800

    boss mikes

--- schoolgame/Mike_hunt_world.gd
+++ schoolgame/Mike_hunt_world.gd
@@ -9,7 +9,6 @@ func _physics_process(delta):
 	if $mikes.get_child_count() == 0 and $boss_mike.spawned == false:
 		$grapple_hook.show()
 		$grapple_hook/CollisionShape2D.disabled = false
-		print("hello")
 		$boss_mike.show()
 		$boss_mike/big_mikeCollisionShape2D2.disabled = false
 		$boss_mike/attack_box/big_mikeCollisionShape2D2.disabled = false
